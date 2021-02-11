61db96b476454032de2cf1d37a7a7c76f84dffbc
shangjingfan <876625909@qq.com>
2021-02-11 23:22:10 +0800

    添加HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 var path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const { title } = require('process');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    // path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin(
+    { title: "abc" }
+  )],
 };
+
+
+
